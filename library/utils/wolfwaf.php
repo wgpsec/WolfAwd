b6ed5dd7c00a241f7d4e4e50922779716906cc80
Adian-kids <adian@wgpsec.org>
2020-10-17 19:46:57 +0800

    :+1:增加了Wolfwaf日志打印功能

--- library/utils/wolfwaf.php
+++ library/utils/wolfwaf.php
@@ -13,6 +13,8 @@ class WolfWaf{
     private $orgin_ip;
     private $requests_data;
     private $requests_method;
+    private $log_file_simple;
+    private $log_file_detailed;
 
     function __construct()
     {
@@ -20,6 +22,9 @@ class WolfWaf{
         $this->requests_method = $_SERVER['REQUEST_METHOD'];
         $this->requests_host = $_SERVER['REMOTE_HOST'];
         $this->orgin_ip = $_SERVER['REMOTE_ADDR'];
+        $this->requests_data = file_get_contents('php://input');
+        $this->log_file_simple = "log_simple.txt";
+        $this->log_file_detailed = "log_detailed.txt";
         $this->requests_log_input(); //请求日志 
         $this->requests_more_input();//详细日志
         $this->wolfwaf_defense();    //防护Function
@@ -29,10 +34,22 @@ class WolfWaf{
         **    Simple Log
         **    Format:txt
         */
-        
-        
-        continue;
-    }
+        $this->contents = date("Y/m/d H:i:s") . "\t";
+        $this->contents .= $this->requests_method . "\t";
+        $this->contents .= $this->orgin_ip . "\t";
+        $this->contents .= $this->requests_url . "\t" ;
+        if ($this->requests_method == 'POST'){
+            $this->contents .= "POST_DATA:" . $this->requests_data . "\n";
+        }
+        else{
+            $this->contents .= "\n";
+        }
+        $log_file = fopen($this->log_file_simple,'a');
+        fwrite($log_file , $this->contents);
+        fclose($log_file);
+        $this->contents = null;
+
+    }   
     function requests_more_input(){
         /* 
         **    Detailed Log
@@ -46,5 +63,8 @@ class WolfWaf{
         */
         continue;
     }
+    
+    
+    
 }
 ?>
