91dae99b4ae1de54973843165aebe91661de0196
Wendell <tongweigii@gmail.com>
2020-11-11 21:25:59 +0800

    获取网络事件,杀进程,阻止curl交flag

---
+++ data/misc/flag_waf/evil.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'header_waf.php';
+echo '1111111';
+readfile('/flag');
+require_once 'footer_waf.php';
---
+++ data/misc/flag_waf/header_waf.php
@@ -0,0 +1,7 @@
+<?php
+
+
+
+
+$___wolfawd_app_start_time = time();
+ob_start();
